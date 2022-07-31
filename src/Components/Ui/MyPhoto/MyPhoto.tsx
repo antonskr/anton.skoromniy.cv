291b46d101dae949d86f97669f24559d24e23492
antonskr <antonskoromniy@gmail.com>
2022-07-31 19:46:25 +0300

    Added filter effect for (MyPhoto)

--- src/Components/Ui/MyPhoto/MyPhoto.tsx
+++ src/Components/Ui/MyPhoto/MyPhoto.tsx
@@ -4,9 +4,20 @@ import styles from './MyPhoto.module.scss'
 
 const MyPhoto = () => {
     return (
-        <div className={styles.photo}>
-            <img src={Photo} alt="photo"/>
-        </div>
+        <>
+            <div className={styles.photo}>
+                <img src={Photo} alt="photo"/>
+            </div>
+            <svg width={0} height={0}>
+                <filter id={'svg_filter'}>
+                    <feTurbulence baseFrequency={0} numOctaves={1}>
+                        <animate attributeName={'baseFrequency'} dur={'1s'} values={'0.04;0'}/>
+                    </feTurbulence>
+                    <feDisplacementMap scale={5} in={'SourceGraphic'}/>
+                </filter>
+            </svg>
+        </>
+
     )
 }
 
