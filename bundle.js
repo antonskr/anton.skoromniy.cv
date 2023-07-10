6020b6b307115efa391fe2d0181a026f22ce3bfd
12345 <antonskoromniy@gmail.com>
2023-07-10 16:31:39 +0300

    Updates

--- bundle.js
+++ bundle.js
@@ -16504,109 +16504,78 @@ var TsParticles = function TsParticles(_ref) {
           enable: false,
           zIndex: 1
         },
-        particles: {
-          number: {
-            value: 10,
-            density: {
-              enable: false,
-              value_area: 800
-            }
-          },
-          color: {
-            value: "#008cff"
-          },
-          shape: {
-            type: "circle"
-          },
-          opacity: {
-            value: 0.8,
-            random: false,
-            anim: {
-              enable: false,
-              speed: 1,
-              opacity_min: 0.1,
-              sync: false
-            }
-          },
-          size: {
-            value: 4,
-            random: false,
-            anim: {
-              enable: false,
-              speed: 40,
-              size_min: 0.1,
-              sync: false
-            }
-          },
-          rotate: {
-            value: 0,
-            random: true,
-            direction: "clockwise",
-            animation: {
-              enable: true,
-              speed: 5,
-              sync: false
-            }
-          },
-          line_linked: {
-            enable: true,
-            distance: 600,
-            color: "#008cff",
-            opacity: 0.4,
-            width: 1
-          },
-          move: {
-            enable: true,
-            speed: 2,
-            direction: "none",
-            random: true,
-            straight: false,
-            out_mode: "out",
-            attract: {
-              enable: false,
-              rotateX: 600,
-              rotateY: 1200
-            }
-          }
-        },
+        fpsLimit: 60,
         interactivity: {
+          detectsOn: 'window',
           events: {
-            onhover: {
+            onClick: {
               enable: true,
-              mode: ["grab"]
+              mode: 'push'
             },
-            onclick: {
-              enable: false,
-              mode: "bubble"
+            onHover: {
+              enable: true,
+              mode: 'repulse'
             },
             resize: true
           },
           modes: {
-            grab: {
-              distance: 400,
-              line_linked: {
-                opacity: 1
-              }
-            },
             bubble: {
               distance: 400,
-              size: 40,
               duration: 2,
-              opacity: 8,
+              opacity: 0.8,
+              size: 40,
               speed: 3
             },
-            repulse: {
-              distance: 200
-            },
             push: {
-              particles_nb: 4
+              quantity: 4
             },
-            remove: {
-              particles_nb: 2
+            repulse: {
+              distance: 200,
+              duration: 0.4
             }
           }
         },
-        retina_detect: true
+        particles: {
+          color: {
+            value: '#008cff'
+          },
+          links: {
+            color: '#008cff',
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1
+          },
+          collisions: {
+            enable: true
+          },
+          move: {
+            direction: 'none',
+            enable: true,
+            outMode: 'bounce',
+            random: false,
+            speed: 2,
+            straight: false
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 800
+            },
+            value: 80
+          },
+          opacity: {
+            value: 0.5
+          },
+          shape: {
+            type: 'circle'
+          },
+          size: {
+            random: true,
+            value: 5
+          }
+        },
+        detectRetina: true
       }
     })
   });
